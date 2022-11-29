53013e72249fd87ca1b25c5a2ded876b343859a7
JeffreyMFarley <jeffrey.m.farley@gmail.com>
2022-11-29 11:25:08 -0500

    Update start script to work from root directory

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-mkdir "$1"
-cd "$1"
-touch input0.txt
-touch input1.txt
-cp ../solve_template.py solve1.py
+set -e
+
+NEW_DIR="./2022/$1"
+
+mkdir "$NEW_DIR"
+touch "$NEW_DIR/input0.txt"
+touch "$NEW_DIR/input1.txt"
+cp ./solve_template.py "$NEW_DIR/solve1.py"
